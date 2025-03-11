7e14aa11e421b61d14d5787b65554f12b9e3cfd1
Omar Alaa Elnahass <21p0197@eng.asu.edu.eg>
2025-03-11 23:11:08 +0200

    update settings.py for vercel deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
